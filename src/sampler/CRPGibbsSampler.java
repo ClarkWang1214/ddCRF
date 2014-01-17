f16c519d33264677920148b692c6d6d17d77b511
Rajarshi Das <rajarshd@cs.cmu.edu>
2014-01-17 14:22:15 -0500

    CRP Gibbs Sampler

---
+++ src/sampler/CRPGibbsSampler.java
@@ -0,0 +1,13 @@
+package sampler;
+
+import Likelihood.Likelihood;
+
+public class CRPGibbsSampler {
+
+	
+	public static int sampleTopic(Likelihood l,int table_index,int list_index)
+	{
+		return 0;
+	}
+	
+}
