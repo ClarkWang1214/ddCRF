e40da86c9d5561365acbec530ce4ac6c6853d7f5
Rajarshi Das <rajarshd@cs.cmu.edu>
2014-01-19 15:12:08 -0500

    city table

---
+++ src/model/CityTable.java
@@ -0,0 +1,29 @@
+package model;
+
+/**
+ * A model to hold the city id and the table_id within the city
+ * @author rajarshd
+ *
+ */
+public class CityTable {
+
+	int city_id;
+	
+	int table_id;
+	
+	
+	public int getCity_id() {
+		return city_id;
+	}
+	public void setCity_id(int city_id) {
+		this.city_id = city_id;
+	}
+	public int getTable_id() {
+		return table_id;
+	}
+	public void setTable_id(int table_id) {
+		this.table_id = table_id;
+	}
+	
+	
+}
