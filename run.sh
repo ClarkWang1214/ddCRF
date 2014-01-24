5ecd9c10028aad39209a62d9d12758d467dfb1b8
JustinCranshaw <jbcranshaw@gmail.com>
2014-01-24 02:55:23 -0500

    modified params

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-n  <num_iter> -v <vocab_size> -d <dirichlet_concentration_param> -s <self_linkage_prob>  ]" 1>&2;
+usage() { echo "Usage: $0 [-n  <num_iter> -v <vocab_size> -d <dirichlet_concentration_param> -s <self_linkage_prob> -t <crp_self_link_prob>]" 1>&2;
           echo -e  "\nnum_iter and vocab_size are required parameters. dirichlet_parameter is defaulted to 0.3 and self_linkage_prob is defaulted to 0.1"       
 
 	exit 1; }
@@ -11,7 +11,7 @@ vocabSize=
 dir_param=0.3 #default value
 self_link_prob=0.1 #default value
 
-while getopts ":n:v:d:s:h" o; do
+while getopts ":n:v:d:s:t:h" o; do
 
 	case "${o}" in
 		
@@ -26,6 +26,9 @@ while getopts ":n:v:d:s:h" o; do
 			;;
 		s)
 			self_link_prob=$OPTARG
+			;;
+		t)
+			crp_self_link_prob=$OPTARG
 			;;
 		h)
 			usage;
@@ -57,4 +60,4 @@ mkdir tables
 mkdir bin
 javac -sourcepath src/ -d bin/ -cp "lib/la4j-0.4.0/bin/la4j-0.4.0.jar:lib/jgrapht-0.8.3/jgrapht-jdk1.6.jar:lib/commons-math3-3.2/commons-math3-3.2.jar" src/Driver.java
 
-java -cp "lib/la4j-0.4.0/bin/la4j-0.4.0.jar:lib/jgrapht-0.8.3/jgrapht-jdk1.6.jar:lib/commons-math3-3.2/commons-math3-3.2.jar:bin" Driver $numIter $vocabSize $dir_param $self_link_prob
+java -cp "lib/la4j-0.4.0/bin/la4j-0.4.0.jar:lib/jgrapht-0.8.3/jgrapht-jdk1.6.jar:lib/commons-math3-3.2/commons-math3-3.2.jar:bin" Driver $numIter $vocabSize $dir_param $self_link_prob $crp_self_link_prob
